09dc93867881b40e76e573e6e039861efef20295
Vladimyr Guedes <vladguedes@hotmail.com>
2023-03-21 18:46:38 -0300

    Verificando se o arquivo existe

--- aluno.c
+++ aluno.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "aluno.h"
 
 struct aluno
@@ -10,9 +11,9 @@ struct aluno
     char documento[20];
 };
 
-Aluno * cria_Aluno(Aluno * aluno)
+Aluno *cria_Aluno(Aluno *aluno)
 {
-    aluno = (Aluno*) malloc(sizeof(Aluno));
+    aluno = (Aluno *)malloc(sizeof(Aluno));
     if (aluno == NULL)
     {
         printf("ERRO!\n");
@@ -25,29 +26,41 @@ Aluno * cria_Aluno(Aluno * aluno)
     printf("Informe o documento do aluno: \n");
     scanf(" %[^\n]s", aluno->documento);
 
-    FILE* aluno_txt;
+    FILE *aluno_txt;
 
-    aluno_txt = fopen("Alunos.txt", "wt");
+    if (access("Alunos.txt", F_OK) == 0)
+    {
 
-    if(aluno_txt == NULL) {
-        exit(1);
-    }
+        aluno_txt = fopen("Alunos.txt", "at");
 
-    fprintf(aluno_txt, "Nome: %s\tMatricula: %d\tDocumento: %s\n", aluno->nome, aluno->matricula, aluno->documento);
+        fprintf(aluno_txt, "Nome: %s\tMatricula: %d\tDocumento: %s\n", aluno->nome, aluno->matricula, aluno->documento);
 
-    fclose(aluno_txt);
+        fclose(aluno_txt);
 
         printf("Aluno criado com sucesso! \n");
+    }
 
-    return(aluno);
+    else
+    {
+
+        aluno_txt = fopen("Alunos.txt", "wt");
+
+        fprintf(aluno_txt, "Nome: %s\tMatricula: %d\tDocumento: %s\n", aluno->nome, aluno->matricula, aluno->documento);
+
+        fclose(aluno_txt);
+
+        printf("Aluno criado com sucesso! \n");
+    }
+
+    return (aluno);
 }
 
-void comb_sort(char * vetor[], int n)
+void comb_sort(char *vetor[], int n)
 {
     int gap = n;
     int busca = 1;
 
-    char * aux;
+    char *aux;
 
     while (gap > 1 || busca == 1)
     {
@@ -57,46 +70,47 @@ void comb_sort(char * vetor[], int n)
         }
         int i = 0;
         busca = 0;
-        while (i + gap< n)
+        while (i + gap < n)
         {
-            if (strcmp(vetor[i], vetor[i+gap]) > 0)
+            if (strcmp(vetor[i], vetor[i + gap]) > 0)
             {
                 aux = vetor[i];
-                vetor[i] = vetor[i+gap];
-                vetor[i+gap] = aux;
+                vetor[i] = vetor[i + gap];
+                vetor[i + gap] = aux;
                 busca = 1;
             }
-
         }
-
     }
-
 }
 
-char* recebe_nomes() {
+char *recebe_nomes()
+{
     char linha[100];
     int i = 0;
-    FILE* aluno_txt;
-    char * nomes[1500];
+    FILE *aluno_txt;
+    char *nomes[1500];
 
     aluno_txt = fopen("Alunos.txt", "rt");
 
-    if(aluno_txt == NULL) {
+    if (aluno_txt == NULL)
+    {
         printf("\nERRO!");
         exit(1);
     }
 
-    while(fgets(linha, 100, aluno_txt) != NULL) {
+    while (fgets(linha, 100, aluno_txt) != NULL)
+    {
         sscanf(linha, " %[^\n]s", &nomes[i * 50]);
         i++;
     }
 
     fclose(aluno_txt);
 
-    return(nomes);
+    return (nomes);
 }
 
-void menu() {
+void menu()
+{
 
     int opc;
     printf("\n====================\n");

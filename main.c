d6c2a210a013c2b2a95be427b151304e55e686c5
ArielTibs <124292071+ArielTibs@users.noreply.github.com>
2023-03-28 16:22:01 -0300

    comentários

--- main.c
+++ main.c
@@ -2,28 +2,28 @@
 #include "aluno.c"
 
 int main(){
-
+// declaração das variaveis e criação dos ponteiros
     int opc;
     Aluno * alunos;
     FILE* teste;
     char nomes[20][50];
     int qnt_linhas;
 
-
+// estrutura de repetição
     while (opc != 2) {
 
 
 
         printf("Digite 1 para criar um aluno, e 2 para fechar o programa: \n");
         scanf("%d", &opc);
-
+// Analisa se o usuário vai criar um aluno
         if (opc == 1) {
             cria_Aluno(alunos);
 
         }
 
     }
-
+//declarando variaveis 
     qnt_linhas = contador();
 
     int i = 0;

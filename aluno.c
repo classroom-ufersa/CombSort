29a3c94e135d0f2d409ff1c7267c5b7e68c570fa
ArielTibs <124292071+ArielTibs@users.noreply.github.com>
2023-03-28 16:20:16 -0300

    Comentários

--- aluno.c
+++ aluno.c
@@ -1,24 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include "aluno.h"
-
+#include <stdio.h> //Biblioteca de entrada e saída.
+#include <stdlib.h> //Biblioteca de funções para alocação dinâmica.
+#include <string.h> //Biblioteca de funções para manipular strings.
+#include <unistd.h> //Biblioteca para criar e acessar ficheiros.
+#include "aluno.h" //Biblioteca criada.
+//implementação da struct alunos.
 struct aluno
 {
     char nome[50];
     char matricula[15];
     char documento[20];
 };
-
+//implementação da função cria_Aluno
 Aluno *cria_Aluno(Aluno *aluno)
 {
+    //Alocando a memoria
     aluno = (Aluno *)malloc(sizeof(Aluno));
     if (aluno == NULL)
     {
         printf("ERRO!\n");
         exit(1);
     }
+     //Imprime as seguintes menssagens na tela do usuário.
+    //Ler as informações digitadas pelo usuário.
     printf("Informe o nome do aluno: \n");
     scanf(" %[^\n]s", aluno->nome);
     printf("Informe a matricula do aluno: \n");
@@ -29,16 +32,16 @@ Aluno *cria_Aluno(Aluno *aluno)
     FILE *aluno_txt;
 
     aluno_txt = fopen("Alunos.txt", "at");
-
+//Escreve os dados que o usuário digitou no arquivo.
     fprintf(aluno_txt, "Nome: %s\tMatricula: %s\tDocumento: %s\n", aluno->nome, aluno->matricula, aluno->documento);
-
+//fecha o arquivo
     fclose(aluno_txt);  
-
+//Imprime as seguintes menssagens na tela do usuário.
     printf("Aluno criado com sucesso! \n");
 
     return (aluno);
 }
-
+//Aplicando no combsort
 void combSort(char lista[][50], int n) {
     FILE* abrir; //c1
     int lacuna = n; //c2
@@ -99,6 +102,6 @@ int contador() {
     }
 
     fclose(abre);
-
+//Retorna um ponteiro para ponteiro, para o primeiro índice da matriz.
     return(numLinhas);
 }
